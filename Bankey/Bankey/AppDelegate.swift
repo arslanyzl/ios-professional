0f1c23c875620731a513d98f905bf6eb61e455fe
Arslan Yazlyyev <arslanyazlyyev@Mahris-MBP.attlocal.net>
2022-04-22 06:22:56 -0500

    summary heaeder

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerviewController.delegate = self
         dummyViewController.logoutDelegate = self
-        window?.rootViewController = mainViewConroller
+        window?.rootViewController = AccountSummaryViewController()
         
         
 //        window?.rootViewController = onboardingContainerviewController
